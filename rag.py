ae85a6c37f694224bc4f2b25ee9579d62809717f
breadbread1984 <breadbread1984@163.com>
2024-05-27 11:19:28 +0800

    update

--- rag.py
+++ rag.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-from os.path import splitext, join
+from os.path import splitext, join, exists
 from shutil import rmtree
-from langchain.document_loaders import UnstructuredPDFLoader
+from langchain.document_loaders import UnstructuredPDFLoader, UnstructuredHTMLLoader, TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 from langchain.vectorstores import Chroma
@@ -12,13 +12,21 @@ from models import Llama3
 from prompts import rag_template
 
 class RAG(object):
-  def __init__(self, pdf_path, db_dir = 'db', locally = False):
+  def __init__(self, file_path, db_dir = 'db', locally = False):
     if exists(db_dir): rmtree(db_dir)
     # load llama3
     tokenizer, llm = Llama3(locally)
     # load pdf to vectordb
     docs = list()
-    loader = UnstructuredPDFLoader(pdf_path, mode = "single", strategy = "fast")
+    stem, ext = splitext(file_path)
+    if ext.lower() == '.pdf':
+      loader = UnstructuredPDFLoader(file_path, mode = "single", strategy = "fast")
+    elif ext.lower() == '.txt':
+      loader = TextLoader(file_path)
+    elif ext.lower() in ['.html', '.htm']:
+      loader = UnstructuredHTMLLoader(file_path)
+    else:
+      raise Exception('unknown file format!')
     docs.append(loader.load())
     text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 150)
     split_docs = text_splitter.split_documents(docs)
@@ -34,3 +42,5 @@ class RAG(object):
   def query(self, question):
     return self.chain({'query': question})
 
+if __name__ == "__main__":
+  rag = RAG('CN117175037\ 固态电解质浆料、固态电解质膜、固态电池及用电装置.html')
