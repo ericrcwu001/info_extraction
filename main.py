254f77590780b9387d98fe5bc1e5ebfa63c56135
breadbread1984 <breadbread1984@163.com>
2024-05-27 14:45:43 +0800

    update

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def main(unused_argv):
       rag = RAG(join(root, f), locally = FLAGS.run_locally)
       neg, _ = rag.query("负极材料属于['碳基','硅基','锂金属或锂合金','氧化物','硫化物']中的哪一类？如果材料不在这些类别中，给出具体材料名称。")
       pos, _ = rag.query("正极材料属于['氧化钴锂','氧化镍锂','氧化锰锂','磷酸亚铁锂','硫化物']中的哪一类？如果材料不在这些类别中，给出具体材料名称。")
-      electrolyte, _ = rag.query("电解质属于['聚合物','氧化物','硫化物']中的哪一类？如果电解质不在这些类别中，给出具体电解质名称。")
+      electrolyte, _ = rag.query("电解质属于['聚合物','氧化物','硫化物']中的哪一类？")
       structure, _ = rag.query("电池结构属于['wound cell','stacked cell']中的哪一类？如果电池结构不在这些类别中，给出具体电池结构名称。")
       company, _ = rag.query("公司名字是什么？")
       content.append({"专利":f,"公司":company,"正极材料":pos,"负极材料":neg,"电解质":electrolyte,"电池结构":structure})
