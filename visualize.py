a1b81663996471fcaaf0a07956c547af2b697ca8
breadbread1984 <breadbread1984@163.com>
2024-05-29 10:36:52 +0800

    update

--- visualize.py
+++ visualize.py
@@ -12,12 +12,12 @@ def add_options():
 def main(unused_argv):
   with open(FLAGS.input_json, 'r') as f:
     results = json.loads(f.read())
-  counts = {'聚合物':0,'氧化物':0,'硫化物':0,'其他':0}
+  counts = {'polymer':0,'oxide':0,'sulfide':0,'others':0}
   for res in results:
-    if res['电解质'].startswith('聚合物'): counts['聚合物'] += 1
-    elif res['电解质'].startswith('氧化物'): counts['氧化物'] += 1
-    elif res['电解质'].startswith('硫化物'): counts['硫化物'] += 1
-    else: counts['其他'] += 1
+    if res['电解质'].startswith('聚合物'): counts['polymer'] += 1
+    elif res['电解质'].startswith('氧化物'): counts['oxide'] += 1
+    elif res['电解质'].startswith('硫化物'): counts['sulfide'] += 1
+    else: counts['others'] += 1
   fig, ax = plt.subplots()
   ax.pie(list(counts.values()), labels = list(counts.keys()))
   plt.savefig('output.png', format = "png")
