57d1ea44104ef3e826adb9369e7f7c00009dbad9
breadbread1984 <breadbread1984@163.com>
2024-06-11 15:46:12 +0800

    update

--- main.py
+++ main.py
@@ -5,29 +5,52 @@ from os.path import splitext, join, exists
 from absl import flags, app
 from tqdm import tqdm
 import json
+from langchain.document_loaders import UnstructuredPDFLoader, UnstructuredHTMLLoader, TextLoader
+from models import Llama2, Llama3, CodeLlama
+
 from rag import RAG
 
 FLAGS = flags.FLAGS
 
 def add_options():
   flags.DEFINE_string('input_dir', default = None, help = 'path to directory containing pdfs')
-  flags.DEFINE_boolean('run_locally', default = False, help = 'whether run LLM locally')
+  flags.DEFINE_boolean('locally', default = False, help = 'whether run LLM locally')
   flags.DEFINE_string('output_json', default = 'output.json', help = 'path to output json')
+  flags.DEFINE_enum('model', default = 'llama3', enum_values = {'llama2', 'llama3', 'codellama'}, help = 'model name')
+  flags.DEFINE_boolean('recursively', default = False, help = 'summary multiple chunks into one summary')
+  flags.DEFINE_string('instruction', default = 'Just focus on starting materials in the given examples, chemical formula of electrolyte and numerical data of conductivity', help = 'extra instruction')
 
 def main(unused_argv):
   content = list()
+  if FLAGS.model == 'llama2':
+    tokenizer, llm = Llama2(FLAGS.locally)
+  elif FLAGS.model == 'llama3':
+    tokenizer, llm = Llama3(FLAGS.locally)
+  elif FLAGS.model == 'codellama':
+    tokenizer, llm = CodeLlama(FLAGS.locally)
+  else:
+    raise Exception('unknown model!')
   for root, dirs, files in tqdm(walk(FLAGS.input_dir)):
     for f in files:
-      print('处理%s' % f)
+      print('1) summarize %s' % f)
       stem, ext = splitext(f)
-      if ext.lower() not in ['.htm', '.html', '.txt']: continue
-      rag = RAG(join(root, f), locally = FLAGS.run_locally)
-      neg, _ = rag.query("负极材料属于['碳基','硅基','锂金属或锂合金','氧化物','硫化物']中的哪一类？如果材料不在这些类别中，给出具体材料名称。尽量精简答案。")
-      pos, _ = rag.query("正极材料属于['氧化钴锂','氧化镍锂','氧化锰锂','磷酸亚铁锂','硫化物']中的哪一类？如果材料不在这些类别中，给出具体材料名称。尽量精简答案。")
-      electrolyte, _ = rag.query("电解质属于['聚合物','氧化物','硫化物']中的哪一类？只回答类别名称或者不知道。")
-      structure, _ = rag.query("电池结构属于['wound cell','stacked cell']中的哪一类？如果电池结构不在这些类别中，给出具体电池结构名称。尽量精简答案")
-      date, _ = rag.query("申请日是什么？尽量精简答案。")
-      content.append({"专利":f,"申请日期":date,"正极材料":pos,"负极材料":neg,"电解质":electrolyte,"电池结构":structure})
+      if ext.lower() in ['.htm', '.html']:
+        loader = UnstructuredHTMLLoader(join(root, f))
+      elif ext.lower() == '.txt':
+        loader = TextLoader(join(root, f))
+      elif ext.lower() == '.pdf':
+        loader = UnstructuredPDFLoader(join(root, f), mode = 'single', strategy = "hi_res")
+      else:
+        raise Exception('unknown format!')
+      text = ''.join([doc.page_content for doc in loader.load()])
+      summary = summarize(text, detail = 0.5, llm = llm, tokenizer = tokenizer, summarize_recursively = FLAGS.recursively, additional_instructions = FLAGS.instruction)
+
+      print('2) RAG with the summarization')
+      rag = RAG(tokenizer, llm, text, locally = FLAGS.locally)
+      formula, _ = rag.query("what is the chemical formula of the electrolyte in the example?")
+      materials, _ = rag.query("what is the starting material in the example?")
+      conductivity, _ = rag.query("what is the conductivity of the electrolyte?")
+      content.append({"patent":f, "chemical formula": formula, "starting materials": materials, "conductivity": conductivity})
   with open(FLAGS.output_json, 'w', encoding = 'utf-8') as f:
     f.write(json.dumps(content, indent = 2, ensure_ascii = False))
 
