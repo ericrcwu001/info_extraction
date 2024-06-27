d72b56c430744db8aa33a455a2a0a3ba257097b3
ericrcwu001 <ericrw2025@student.cis.edu.hk>
2024-06-27 16:18:13 +0800

    day 3

--- main.py
+++ main.py
@@ -5,57 +5,79 @@ from os.path import splitext, join, exists
 from absl import flags, app
 from tqdm import tqdm
 import json
-from langchain.document_loaders import UnstructuredPDFLoader, UnstructuredHTMLLoader, TextLoader
+from langchain_community.document_loaders import UnstructuredPDFLoader, UnstructuredHTMLLoader, TextLoader
 from models import Llama2, Llama3, CodeLlama
 from summarize import summarize
 from rag import RAG
 
 FLAGS = flags.FLAGS
 
+
 def add_options():
-  flags.DEFINE_string('input_dir', default = None, help = 'path to directory containing pdfs')
-  flags.DEFINE_boolean('locally', default = False, help = 'whether run LLM locally')
-  flags.DEFINE_string('output_json', default = 'output.json', help = 'path to output json')
-  flags.DEFINE_enum('model', default = 'llama3', enum_values = {'llama2', 'llama3', 'codellama'}, help = 'model name')
-  flags.DEFINE_boolean('recursively', default = True, help = 'summary multiple chunks into one summary')
-  flags.DEFINE_string('instruction', default = 'Focus on details of examples, chemical formula of electrolyte and numerical data of conductivity', help = 'extra instruction')
+    flags.DEFINE_string('input_dir', default="test", help='path to directory containing pdfs')
+    flags.DEFINE_boolean('locally', default=False, help='whether run LLM locally')
+    flags.DEFINE_string('output_json', default='output.json', help='path to output json')
+    flags.DEFINE_enum('model', default='llama2', enum_values={'llama2', 'llama3', 'codellama'}, help='model name')
+    flags.DEFINE_boolean('recursively', default=True, help='summary multiple chunks into one summary')
+    flags.DEFINE_string('instruction',
+                        default='Focus on details of examples, chemical formula of electrolyte and numerical data of '
+                                'conductivity',
+                        help='extra instruction')
+
 
 def main(unused_argv):
-  content = list()
-  if FLAGS.model == 'llama2':
-    tokenizer, llm = Llama2(FLAGS.locally)
-  elif FLAGS.model == 'llama3':
-    tokenizer, llm = Llama3(FLAGS.locally)
-  elif FLAGS.model == 'codellama':
-    tokenizer, llm = CodeLlama(FLAGS.locally)
-  else:
-    raise Exception('unknown model!')
-  for root, dirs, files in tqdm(walk(FLAGS.input_dir)):
-    for f in files:
-      stem, ext = splitext(f)
-      if ext.lower() in ['.htm', '.html']:
-        loader = UnstructuredHTMLLoader(join(root, f))
-      elif ext.lower() == '.txt':
-        loader = TextLoader(join(root, f))
-      elif ext.lower() == '.pdf':
-        loader = UnstructuredPDFLoader(join(root, f), mode = 'single', strategy = "hi_res")
-      else:
-        raise Exception('unknown format!')
-      text = ''.join([doc.page_content for doc in loader.load()])
-      print('1) summarize %s' % f)
-      summary = summarize(text, detail = 0.5, llm = llm, tokenizer = tokenizer, summarize_recursively = FLAGS.recursively, additional_instructions = FLAGS.instruction)
+    content = list()
+    if FLAGS.model == 'llama2':
+        tokenizer, llm = Llama2(FLAGS.locally)
+    elif FLAGS.model == 'llama3':
+        tokenizer, llm = Llama3(FLAGS.locally)
+    elif FLAGS.model == 'codellama':
+        tokenizer, llm = CodeLlama(FLAGS.locally)
+    else:
+        raise Exception('unknown model!')
 
-      print('2) RAG with the summarization')
-      rag_long = RAG(tokenizer, llm, text, locally = FLAGS.locally, db_dir = "long")
-      rag_short = RAG(tokenizer, llm, summary, locally = FLAGS.locally, db_dir = "short")
-      formula, _ = rag_short.query("what is the chemical formula of the electrolyte produced in the example?")
-      materials, _ = rag_short.query("what are the materials used in the example?")
-      conductivity, _ = rag_long.query("what is the conductivity of the electrolyte?")
-      content.append({"patent":f, "summary": summary, "chemical formula": formula, "starting materials": materials, "conductivity": conductivity})
-  with open(FLAGS.output_json, 'w', encoding = 'utf-8') as f:
-    f.write(json.dumps(content, indent = 2, ensure_ascii = False))
+    file_count = sum(len(files) for _, _, files in walk(FLAGS.input_dir))  # Get the number of files
+
+    with tqdm(total=file_count) as pbar:
+        for root, dirs, files in walk(FLAGS.input_dir):
+            # print(FLAGS.input_dir)
+
+            for f in files:
+                stem, ext = splitext(f)
+                if ext.lower() in ['.htm', '.html']:
+                    loader = UnstructuredHTMLLoader(join(root, f))
+                elif ext.lower() == '.txt':
+                    loader = TextLoader(join(root, f))
+                elif ext.lower() == '.pdf':
+                    loader = UnstructuredPDFLoader(join(root, f), mode='single', strategy="hi_res")
+                else:
+                    raise Exception('unknown format!')
+
+                loaded = loader.load()
+                for doc in loaded:
+                    print(doc.page_content)
+
+                # text = ''.join([doc.page_content for doc in loader.load()])
+                print('1) summarize %s' % f)
+                summary = summarize(text, detail=0.5, llm=llm, tokenizer=tokenizer, summarize_recursively=FLAGS.recursively,
+                                    additional_instructions=FLAGS.instruction)
+                #
+                # print('2) RAG with the summarization')
+                # rag_long = RAG(tokenizer, llm, text, locally=FLAGS.locally, db_dir="long")
+                # rag_short = RAG(tokenizer, llm, summary, locally=FLAGS.locally, db_dir="short")
+                # formula, _ = rag_short.query("what is the chemical formula of the electrolyte produced in the example?")
+                # materials, _ = rag_short.query("what are the materials used in the example?")
+                # conductivity, _ = rag_long.query("what is the conductivity of the electrolyte?")
+                # content.append(
+                #     {"patent": f, "summary": summary, "chemical formula": formula, "starting materials": materials,
+                #      "conductivity": conductivity})
+                #
+                pbar.update(1)
+
+    with open(FLAGS.output_json, 'w', encoding='utf-8') as f:
+        f.write(json.dumps(content, indent=2, ensure_ascii=False))
+
 
 if __name__ == "__main__":
-  add_options()
-  app.run(main)
-
+    add_options()
+    app.run(main)
--- rag.py
+++ rag.py
@@ -2,42 +2,51 @@
 
 from os.path import splitext, join, exists
 from shutil import rmtree
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings.huggingface import HuggingFaceEmbeddings
-from langchain.vectorstores import Chroma
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings.huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
 from langchain_core.vectorstores import VectorStoreRetriever
 from langchain.chains import RetrievalQA
 from models import Llama3
 from prompts import rag_template
 
+
 class RAG(object):
-  def __init__(self, tokenizer, llm, text, db_dir = 'db', locally = False):
-    if exists(db_dir): rmtree(db_dir)
-    # load pdf to vectordb
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap = 150)
-    split_texts = text_splitter.split_text(text)
-    embeddings = HuggingFaceEmbeddings(model_name = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
-    vectordb = Chroma.from_texts(
-        texts = split_texts,
-        embedding = embeddings,
-        persist_directory = db_dir)
-    vectordb.persist()
-    # create chain
-    prompt = rag_template(tokenizer)
-    self.chain = RetrievalQA.from_chain_type(llm, retriever = vectordb.as_retriever(), return_source_documents = True, chain_type_kwargs = {"prompt": prompt})
-  def query(self, question):
-    res = self.chain({'query': question})
-    return res['result'].replace('assistant\n\n',''), res['source_documents']
+    def __init__(self, tokenizer, llm, text, db_dir='db', locally=False):
+        if exists(db_dir): rmtree(db_dir)
+        # load pdf to vectordb
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=150)
+        split_texts = text_splitter.split_text(text)
+        embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2")
+        vectordb = Chroma.from_texts(
+            texts=split_texts,
+            embedding=embeddings,
+            persist_directory=db_dir)
+        vectordb.persist()
+        # create chain
+        prompt = rag_template(tokenizer)
+        self.chain = RetrievalQA.from_chain_type(llm, retriever=vectordb.as_retriever(), return_source_documents=True,
+                                                 chain_type_kwargs={"prompt": prompt})
+
+    def query(self, question):
+        res = self.chain({'query': question})
+        return res['result'].replace('assistant\n\n', ''), res['source_documents']
+
 
 if __name__ == "__main__":
-  rag = RAG('CN117175037 固态电解质浆料、固态电解质膜、固态电池及用电装置.html', locally = True)
-  res, support = rag.query("负极材料属于['碳基','硅基','锂金属或锂合金','氧化物','硫化物']中的哪一类？如果材料不在这些类别中，给出具体材料名称。")
-  print('负极：',res, support)
-  res, support = rag.query("正极材料属于['氧化钴锂','氧化镍锂','氧化锰锂','磷酸亚铁锂','硫化物']中的哪一类？如果材料不在这些类别中，给出具体材料名称。")
-  print('正极：',res, support)
-  res, support = rag.query("电解质属于['聚合物','氧化物','硫化物']中的哪一类？如果电解质不在这些类别中，给出具体电解质名称。")
-  print('电解质：',res, support)
-  res, support = rag.query("电池结构属于['wound cell','stacked cell']中的哪一类？如果电池结构不再这些类别中，给出具体电池结构名称。")
-  print('电池结构：',res, support)
-  res, support = rag.query("公司名字是什么？")
-  print("公司名字：",res, support)
+    print("hi")
+    # rag = RAG('CN117175037 固态电解质浆料、固态电解质膜、固态电池及用电装置.html', locally=True)
+    # res, support = rag.query(
+    #     "负极材料属于['碳基','硅基','锂金属或锂合金','氧化物','硫化物']中的哪一类？如果材料不在这些类别中，给出具体材料名称。")
+    # print('负极：', res, support)
+    # res, support = rag.query(
+    #     "正极材料属于['氧化钴锂','氧化镍锂','氧化锰锂','磷酸亚铁锂','硫化物']中的哪一类？如果材料不在这些类别中，给出具体材料名称。")
+    # print('正极：', res, support)
+    # res, support = rag.query(
+    #     "电解质属于['聚合物','氧化物','硫化物']中的哪一类？如果电解质不在这些类别中，给出具体电解质名称。")
+    # print('电解质：', res, support)
+    # res, support = rag.query(
+    #     "电池结构属于['wound cell','stacked cell']中的哪一类？如果电池结构不再这些类别中，给出具体电池结构名称。")
+    # print('电池结构：', res, support)
+    # res, support = rag.query("公司名字是什么？")
+    # print("公司名字：", res, support)
